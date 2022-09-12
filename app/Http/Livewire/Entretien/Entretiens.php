650bf240e6d0351d071c2739847e1ffb2db4ce23
unknown <ashuzamaheshe4@gmail.com>
2022-09-12 21:53:52 +0200

    Ash commit

--- app/Http/Livewire/Entretien/Entretiens.php
+++ app/Http/Livewire/Entretien/Entretiens.php
@@ -2,12 +2,46 @@
 
 namespace App\Http\Livewire\Entretien;
 
+use App\Models\Entretien;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Entretiens extends Component
 {
+    use WithPagination;
+
+
+    public $perpage = 4;
+
+    public $iddelete = NULL;
+    public $selbyname = NULL;
+    public $selbypriority = NULL;
+
+    public function delete(){
+        $var=Entretien::whereId($this->iddelete)->delete();
+        if($var){
+            $this->dispatchBrowserEvent('ok', [
+                'message'=>'<b>Succès</b><br/><i>Suppresion effectuee</i>',
+            ]);
+            $refresh;
+        }else{
+            $this->dispatchBrowserEvent('ok', [
+                'message'=>'<b>Désoler</b><br/><i>Echèc de suppression</i>',
+            ]);
+            $refresh;
+        }
+    }
+
+    public function alertsupr($id){
+        $this->iddelete = $id;
+    }
+
+    public function reload(){
+        $refresh;
+    }
     public function render()
     {
-        return view('livewire.entretien.entretiens');
+        $entretiens = Entretien::all();
+        return view('livewire.entretien.entretiens', ['entretiens' => $entretiens]);
     }
 }
--- app/Http/Livewire/TypeMobile/TypeMobiles.php
+++ app/Http/Livewire/TypeMobile/TypeMobiles.php
@@ -17,26 +17,73 @@ class TypeMobiles extends Component
     public $selbyname = NULL;
     public $selbypriority = NULL;
 
-    public function delete(){
-        $var=Type_Mobile::whereId($this->iddelete)->delete();
-        if($var){
+    public $designation;
+
+    protected $rules = [
+        'designation' => 'required',
+    ];
+
+    protected $messages = [
+        'designation.required' => 'la designation est obligatoire',
+    ];
+
+    // vider les champs
+    public function resetFields()
+    {
+        $this->designation = '';
+    }
+
+
+    // realtime validation
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
+    }
+    public function save()
+    {
+        $this->validate();
+        try {
+            Type_Mobile::create([
+                'designation' => ucfirst(trans($this->designation)),
+            ])->save();
+            // Set Flash Message
             $this->dispatchBrowserEvent('ok', [
-                'message'=>'<b>Succès</b><br/><i>Suppresion effectuee</i>',
+                'message' => '<b>Succès</b><br/><span style="color: #2d3354; ">Avocat enregistré</span>',
+            ]);
+            $this->resetFields();
+        } catch (\Exception $e) {
+            // Set Flash Message
+            $this->dispatchBrowserEvent('fail', [
+                'message' => "Quelque chose ne va pas lors de l'enregistrement du Client'!! " . $e->getMessage()
+            ]);
+            // Reset Form Fields After Creating departement
+            $this->resetFields();
+        }
+    }
+
+    public function delete()
+    {
+        $var = Type_Mobile::whereId($this->iddelete)->delete();
+        if ($var) {
+            $this->dispatchBrowserEvent('ok', [
+                'message' => '<b>Succès</b><br/><i>Suppresion effectuee</i>',
             ]);
             $refresh;
-        }else{
+        } else {
             $this->dispatchBrowserEvent('ok', [
-                'message'=>'<b>Désoler</b><br/><i>Echèc de suppression</i>',
+                'message' => '<b>Désoler</b><br/><i>Echèc de suppression</i>',
             ]);
             $refresh;
         }
     }
 
-    public function alertsupr($id){
+    public function alertsupr($id)
+    {
         $this->iddelete = $id;
     }
 
-    public function reload(){
+    public function reload()
+    {
         $refresh;
     }
     public function render()
