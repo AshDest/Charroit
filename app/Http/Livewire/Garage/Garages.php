33268e3c383b6197a7d74c9ca4d7ad7318eac480
unknown <ashuzamaheshe4@gmail.com>
2022-09-13 12:38:43 +0200

    Ash commit

--- app/Http/Livewire/Garage/Garages.php
+++ app/Http/Livewire/Garage/Garages.php
@@ -2,12 +2,133 @@
 
 namespace App\Http\Livewire\Garage;
 
+use App\Models\Garage;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Garages extends Component
 {
+    use WithPagination;
+
+
+    public $perpage = 4;
+
+    public $iddelete = NULL;
+    public $selbyname = NULL;
+    public $selbypriority = NULL;
+
+    public $nomgarage;
+    public $contact;
+    public $adresse;
+
+    public $updates=false;
+
+    public $search;
+
+    protected $rules = [
+        'nomgarage' => 'required',
+        'contact' => 'required',
+        'adresse' => 'required',
+    ];
+
+    protected $messages = [
+        'nomgarage.required' => 'La nommination est obligatoire',
+        'contact.required' => 'Contact obligatoire',
+        'adresse.required' => 'Adresse est obligatoire',
+    ];
+
+    // vider les champs
+    public function resetFields()
+    {
+        $this->nomgarage = '';
+        $this->contact = '';
+        $this->adresse = '';
+    }
+
+    public function save()
+    {
+        $this->validate();
+        try {
+            Garage::create([
+                'nomgarage' => ucfirst(trans($this->nomgarage)),
+                'contact' => $this->contact,
+                'adresse' => $this->adresse,
+            ])->save();
+            // Set Flash Message
+            $this->dispatchBrowserEvent('ok', [
+                'message' => '<b>Succès</b><br/><span style="color: #2d3354; ">Section enregistré</span>',
+            ]);
+            $this->resetFields();
+        } catch (\Exception $e) {
+            // Set Flash Message
+            $this->dispatchBrowserEvent('fail', [
+                'message' => "Quelque chose ne va pas lors de l'enregistrement'!! " . $e->getMessage()
+            ]);
+            // Reset Form Fields After Creating departement
+            $this->resetFields();
+        }
+    }
+
+    public function delete($id){
+        $var=Garage::whereId($id)->delete();
+        if($var){
+            $this->dispatchBrowserEvent('ok', [
+                'message'=>'<b>Succès</b><br/><i>Suppresion effectuee</i>',
+            ]);
+            $refresh;
+        }else{
+            $this->dispatchBrowserEvent('ok', [
+                'message'=>'<b>Désoler</b><br/><i>Echèc de suppression</i>',
+            ]);
+            $refresh;
+        }
+    }
+
+    public function edit($id){
+        $this->updates = true;
+        $var = Garage::find($id);
+        $this->ids = $id;
+        $this->nomgarage = $var->nomgarage;
+        $this->contact = $var->contact;
+        $this->adresse = $var->adresse;
+
+    }
+    public function updates(){
+        $this->validate();
+        Garage::whereId($this->ids)->update([
+            'nomgarage' => ucfirst(trans($this->nomgarage)),
+            'contact' => $this->contact,
+            'adresse' => $this->adresse,
+        ]);
+        $this->resetFields();
+        $this->dispatchBrowserEvent('ok', [
+            'message'=>'<b>Succès</b><br/><span style="color: #2d3354; ">Section modifié</span>',
+        ]);
+    }
+
+    public function alertsupr($id){
+        $this->iddelete = $id;
+    }
+    public function reload(){
+        $refresh;
+    }
+
+    public function reload(){
+        $refresh;
+    }
     public function render()
     {
-        return view('livewire.garage.garages');
+        return view('livewire.garage.garages', [
+            'garages' => Garage::where(
+            'nomgarage',
+            'LIKE',
+            '%' . $this->search . '%'
+        )
+            ->orWhere(
+                'id',
+                'LIKE',
+                '%' . $this->search . '%'
+            )
+            ->orderBy('id', 'ASC')->paginate($this->perpage),]);
     }
 }
--- resources/views/livewire/garage/garages.blade.php
+++ resources/views/livewire/garage/garages.blade.php
@@ -1,3 +1,152 @@
 <div>
-    {{-- If your happiness depends on money, you will never be happy with yourself. --}}
-</div>
+    <!-- START: Main Content-->
+    <main>
+     <div class="container-fluid site-width">
+         <!-- START: Breadcrumbs-->
+         <div class="row ">
+             <div class="col-12  align-self-center">
+                 <div class="sub-header mt-3 py-3 align-self-center d-sm-flex w-100 rounded">
+                     <div class="w-sm-100 mr-auto"><h4 class="mb-0">Section</h4></div>
+                     <ol class="breadcrumb bg-transparent align-self-center m-0 p-0">
+                         <li class="breadcrumb-item">Acceuil</li>
+                         <li class="breadcrumb-item active">Sections</li>
+                     </ol>
+                 </div>
+             </div>
+         </div>
+         <!-- END: Breadcrumbs-->
+
+         <!-- START: Card Data-->
+         <div class="row">
+
+             <div class="col-12 col-lg-6 mt-3">
+                 <div class="card">
+                     <div class="card-header">
+                         @if ($updates)
+                         <h4 class="card-title">Modifier Section</h4>
+                         @else
+                         <h4 class="card-title">Ajouter Section</h4>
+                         @endif
+                     </div>
+                     <div class="card-content">
+                         <div class="card-body">
+                             <div class="row">
+                                 <div class="col-12">
+                                     @if ($updates)
+                                     <form wire:submit.prevent="updates">
+                                         <div class="form-group row">
+                                             <label for="designation" class="col-sm-4 col-form-label">Section</label>
+                                             <div class="col-sm-8">
+                                                 <input type="text" class="form-control" id="designation"wire:model="designation"
+                                                  placeholder="Description type automobile">
+                                                  @error('designation')
+                                                 <span style="color: red;">{{ $message }}</span>
+                                                @enderror
+                                             </div>
+                                         </div>
+                                         <div class="form-group row">
+                                             <div class="col-sm-10">
+                                                 <button type="submit" class="btn btn-primary">
+                                                    <span class="icon-plus"></span>  Modifier</button>
+                                             </div>
+                                         </div>
+                                     </form>
+                                 @else
+                                 <form wire:submit.prevent="save">
+                                     <div class="form-group row">
+                                         <label for="designation" class="col-sm-4 col-form-label">Section</label>
+                                         <div class="col-sm-8">
+                                             <input type="text" class="form-control" id="designation" wire:model="designation"
+                                              placeholder="Description type automobile">
+                                              @error('designation')
+                                                 <span style="color: red;">{{ $message }}</span>
+                                                @enderror
+                                         </div>
+                                     </div>
+                                     <div class="form-group row">
+                                         <div class="col-sm-10">
+                                             <button type="submit" class="btn btn-primary">
+                                                 <span class="icon-plus"></span>  Enregistrer</button>
+                                         </div>
+                                     </div>
+                                 </form>
+                                 @endif
+                                 </div>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+
+             <div class="col-12 col-sm-6 mt-3">
+                 <div class="card">
+                     <div class="card-header d-flex justify-content-between align-items-center">
+                         <h4 class="card-title">Liste Sections</h4>
+                         <div class="col-sm-5">
+                             <input type="text" id="reseach" placeholder="recherche une categclient" class="form-control"
+                             wire:model="categclient_reseach">
+                         </div>
+                         <div class="col-auto ml-auto sm-2">
+                             Afficher
+                             <select id="page_active" wire:model.lazy="page_active" class="custom-select w-auto">
+                                 @for ($i = 3; $i <= 9; $i += 3)
+                                     <option value="{{ $i }}">{{ $i }}</option>
+                                 @endfor
+                             </select>
+                             <label for="page_active">Page</label>
+                         </div>
+                     </div>
+                     <div class="card-body">
+                         <div class="table-responsive">
+                             <table class="table">
+                                 <thead>
+                                     <tr>
+                                         <th scope="col">ID</th>
+                                         <th scope="col">Designation Section</th>
+                                         <th scope="col">Modifier</th>
+                                         <th scope="col">Suprimer</th>
+                                     </tr>
+                                 </thead>
+                                 <tbody>
+                                     @forelse($sections as $key=> $section)
+                                     <tr>
+                                         <th scope="row">{{$key+1}}</th>
+                                         <td>{{ $section->designation }}</td>
+                                         <td>
+                                             <button
+                                                 class="btn btn-primary btn-sm"  wire:click="edit({{ $section->id }})">
+                                                 <span class="icon-pencil" style="cursor: pointer"></span></span></button>
+                                         </td>
+                                         <td>
+                                         <button
+                                             class="btn btn-danger btn-sm" wire:click="delete({{ $section->id }})">
+                                             <span class="icon-trash" style="cursor: pointer"></span></span>
+                                         </button>
+                                         </td>
+                                     </tr>
+                                     @empty
+                                 <tr>
+                                     <td class="alert alert-danger" colspan="12">
+                                         <center>Aucune information</center>
+                                     </td>
+                                 </tr>
+                             @endforelse
+
+                                 </tbody>
+                             </table>
+                             @if (count($sections))
+                             {{$sections->links('vendor.livewire.bootstrap')}}
+                             @endif
+                         </div>
+                     </div>
+                 </div>
+
+             </div>
+
+
+         </div>
+         <!-- END: Card DATA-->
+     </div>
+ </main>
+ <!-- END: Content-->
+ </div>
