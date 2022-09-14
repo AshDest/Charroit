b73506fd99e28a3a680c74985ae31af1e7baf0ae
unknown <ashuzamaheshe4@gmail.com>
2022-09-15 00:07:01 +0200

    Ash commit

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,48 +1,56 @@
 <!-- START: Main Menu-->
-        <div class="sidebar" style="background-color: #e6e4e3; ">
-            <div class="site-width">
-                <!-- START: Menu-->
-                <ul id="side-menu" class="sidebar-menu">
-                    <li class="dropdown active" class="colorText"><a href="/">
-                        <i class="fas fa-warehouse mr-1" style="color: #9dced4; ">
+<div class="sidebar" style="background-color: #e6e4e3; ">
+    <div class="site-width">
+        <!-- START: Menu-->
+        <ul id="side-menu" class="sidebar-menu">
+            <li class="dropdown active" class="colorText"><a href="/">
+                    <i class="fas fa-warehouse mr-1" style="color: #9dced4; ">
                     </i><span class="colorText">Tableau de bord</span></a></li>
-                    <li class="dropdown"><a href="#">
-                        <i class="fas fa-users mr-1" style="color: #9dced4; "></i>
-                        <span class="colorText">Entretien</span></a>
-                        <ul>
-                            <li><a href="/addentretien">&nbsp;&nbsp;<i class="fas fa-user-plus"></i>Ajouter</a></li>
-                            <li><a href="/entretien">&nbsp;&nbsp;<i class="icon-book-open"></i> Liste</a></li>
-                        </ul>
-                    </li>
-                    <li class="dropdown"><a href="#">
-                        <i class="fas fa-users mr-1" style="color: #9dced4; "></i>
-                        <span class="colorText">Prelevement</span></a>
-                        <ul>
-                            <li><a href="/addprelevement">&nbsp;&nbsp;<i class="fas fa-user-plus"></i>Ajouter</a></li>
-                            <li><a href="/prelevement">&nbsp;&nbsp;<i class="icon-book-open"></i>Liste</a></li>
-                        </ul>
-                    </li>
-
+            <li class="dropdown"><a href="#">
+                    <i class="fas fa-users mr-1" style="color: #9dced4; "></i>
+                    <span class="colorText">Entretien</span></a>
+                <ul>
+                    <li><a href="/addentretien">&nbsp;&nbsp;<i class="fas fa-user-plus"></i>Ajouter</a></li>
+                    <li><a href="/entretien">&nbsp;&nbsp;<i class="icon-book-open"></i> Liste</a></li>
                 </ul>
-                <!-- END: Menu-->
-                <ol class="breadcrumb bg-transparent align-self-center m-0 p-0 ml-auto">
-                    <li class="breadcrumb-item"><a href="#">Application</a></li>
-                    <li class="breadcrumb-item active">Dashboard</li>
-                </ol>
-            </div>
-        </div>
-        <!-- END: Main Menu-->
+            </li>
+            <li class="dropdown"><a href="#">
+                    <i class="fas fa-users mr-1" style="color: #9dced4; "></i>
+                    <span class="colorText">Prelevement</span></a>
+                <ul>
+                    <li><a href="/addprelevement">&nbsp;&nbsp;<i class="fas fa-user-plus"></i>Ajouter</a></li>
+                    <li><a href="/prelevement">&nbsp;&nbsp;<i class="icon-book-open"></i> Liste</a></li>
+                </ul>
+            </li>
+            <li class="dropdown"><a href="#">
+                <i class="fas fa-car mr-1" style="color: #9dced4; "></i>
+                <span class="colorText">Véhicule</span></a>
+            <ul>
+                <li><a href="/addmobiles">&nbsp;&nbsp;<i class="fas fa-car"></i>Ajouter</a></li>
+                <li><a href="/mobiles">&nbsp;&nbsp;<i class="fas fa-clipboard-list"></i> Liste</a></li>
+            </ul>
+        </li>
+
+        </ul>
+        <!-- END: Menu-->
+        <ol class="breadcrumb bg-transparent align-self-center m-0 p-0 ml-auto">
+            <li class="breadcrumb-item"><a href="#">Application</a></li>
+            <li class="breadcrumb-item active">Dashboard</li>
+        </ol>
+    </div>
+</div>
+<!-- END: Main Menu-->
 ass="icon-tag"></i> Rapport</a></li>
-                            <li><a href="#">&nbsp;&nbsp;<i class="icon-book-open"></i> Autre rapport</a></li>
-                        </ul>
-                    </li>
+<li><a href="#">&nbsp;&nbsp;<i class="icon-book-open"></i> Autre rapport</a></li>
+</ul>
+</li>
 
-                </ul>
-                <!-- END: Menu-->
-                <ol class="breadcrumb bg-transparent align-self-center m-0 p-0 ml-auto">
-                    <li class="breadcrumb-item"><a href="#">Application</a></li>
-                    <li class="breadcrumb-item active">Dashboard</li>
-                </ol>
-            </div>
-        </div>
-        <!-- END: Main Menu-->
+</ul>
+<!-- END: Menu-->
+<ol class="breadcrumb bg-transparent align-self-center m-0 p-0 ml-auto">
+    <li class="breadcrumb-item"><a href="#">Application</a></li>
+    <li class="breadcrumb-item active">Dashboard</li>
+</ol>
+</div>
+</div>
+<!-- END: Main Menu-->
