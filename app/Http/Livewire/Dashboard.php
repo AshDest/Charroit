173a0b0c5345e78b274e3275e803a492c13d34c8
unknown <ashuzamaheshe4@gmail.com>
2022-09-15 13:49:28 +0200

    Ash commit

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -2,12 +2,28 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Mobile;
+use App\Models\Type_Mobile;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Dashboard extends Component
 {
+    use WithPagination;
+
+
+    public $perpage = 4;
+    public $types;
+    // public $mobiles;
+    public function mount()
+    {
+        // $this->types = Type_Mobile::all();
+    }
     public function render()
     {
-        return view('livewire.dashboard');
+        return view('livewire.dashboard', [
+            'mobiles' => Mobile::orderBy('created_at', 'DESC')
+            ->paginate($this->perpage),
+        ]);
     }
 }
